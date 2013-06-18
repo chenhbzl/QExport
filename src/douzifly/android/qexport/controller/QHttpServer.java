36acc49d633ec88c879d49fa026098b0e0aab876
douzifly <douzifly@gmail.com>
2013-06-18 21:41:58 +0800

    add http lib

---
+++ src/douzifly/android/qexport/controller/QHttpServer.java
@@ -0,0 +1,23 @@
+/**
+ * douzifly @Jun 18, 2013
+ * github.com/douzifly
+ * douzifly@gmail.com
+ */
+package douzifly.android.qexport.controller;
+
+/**
+ * 
+ * 提供文件传输支持
+ * @author douzifly
+ *
+ */
+public class QHttpServer {
+    
+    public void start(){
+        
+    }
+    
+    public void stop(){
+        
+    }
+}
